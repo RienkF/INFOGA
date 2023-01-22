d62567cedc73d4e66c1741600c04088561c021c8
Rienk <rienkfidder@gmail.com>
2023-01-22 12:52:47 +0100

    Semi correct, but not for 3 points on a line and on the convex hull

--- ConvexHull/ConvexHull/Program.cs
+++ ConvexHull/ConvexHull/Program.cs
@@ -1,10 +1,19 @@
 ﻿Random rnd = new();
 
-// See https://aka.ms/new-console-template for more information
+const int leftBound = -30;
+const int rightBound = 30;
+const int topBound = 15;
+const int bottomBound = -15;
+
 Console.WriteLine("INFOGA | Convex Hull | Rienk Fidder - Merijn Schepers");
 
 IEnumerable<Point> points = GetPoints();
 Algorithm algorithm = ChooseAlgorithm();
+
+Console.WriteLine("Points:");
+
+foreach (Point point in points)
+    Console.WriteLine($"{point.X}, {point.Y}");
 
 IEnumerable<Point> convexHull = algorithm switch
 {
@@ -12,7 +21,14 @@ IEnumerable<Point> convexHull = algorithm switch
     _ => throw new NotImplementedException()
 };
 
+Console.WriteLine("Convex Hull:");
 
+foreach (Point point in convexHull) 
+    Console.WriteLine($"{point.X}, {point.Y}");
+
+Console.WriteLine("Image:");
+
+DrawHull(points, convexHull);
 
 IEnumerable<Point> GetPoints()
 {
@@ -25,7 +41,7 @@ IEnumerable<Point> GetPoints()
         input = Console.ReadLine();
 
 
-    return new List<Point>(n).Select(_ => new Point(rnd.Next(-100, 100), rnd.Next(-100, 100)));
+    return new int[n].Select(_ => new Point(rnd.Next(leftBound, rightBound), rnd.Next(bottomBound, topBound))).Distinct().ToList();
 }
 
 Algorithm ChooseAlgorithm()
@@ -48,20 +64,47 @@ Algorithm ChooseAlgorithm()
 IEnumerable<Point> GrahamScan(IEnumerable<Point> points)
 {
     IEnumerable<Point> sortedPoints = points.OrderBy(p => p.X).ToList();
+    IEnumerable<Point> reversedPoints = sortedPoints.Reverse().ToList();
 
-    IEnumerable<Point> lUpper = new List<Point>();
+
+    IEnumerable<Point> upper = new List<Point>();
+    IEnumerable<Point> lower = new List<Point>();
 
     for (int i = 0; i < sortedPoints.Count(); i++)
     {
-        lUpper = lUpper.Append(sortedPoints.ElementAt(i));
+        lower = lower.Append(sortedPoints.ElementAt(i)).ToList();
         if (i < 2)
             continue;
 
         bool done = false;
 
-        while (!done)
+        while (!done || lower.Count() < 3)
         {
-            IEnumerable<Point> last3Points = lUpper.TakeLast(3);
+            IEnumerable<Point> last3Points = lower.TakeLast(3).ToList();
+
+            Point a = last3Points.First();
+            Point b = last3Points.ElementAt(1);
+            Point c = last3Points.Last();
+
+            // https://math.stackexchange.com/questions/1324179/how-to-tell-if-3-connected-points-are-connected-clockwise-or-counter-clockwise 
+            // Second answer, check if counterclockwise, but strictly
+            if (b.X * a.Y + c.X * b.Y + a.X * c.Y <= a.X * b.Y + b.X * c.Y + c.X * a.Y)
+                lower = lower.Except(new List<Point> { b }).ToList();
+            else done = true;
+        }
+    }
+
+    for (int i = 0; i < reversedPoints.Count(); i++)
+    {
+        upper = upper.Append(reversedPoints.ElementAt(i)).ToList();
+        if (i < 2)
+            continue;
+
+        bool done = false;
+
+        while (!done || lower.Count() < 3)
+        {
+            IEnumerable<Point> last3Points = upper.TakeLast(3).ToList();
 
             Point a = last3Points.First();
             Point b = last3Points.ElementAt(1);
@@ -69,15 +112,50 @@ IEnumerable<Point> GrahamScan(IEnumerable<Point> points)
 
             // https://math.stackexchange.com/questions/1324179/how-to-tell-if-3-connected-points-are-connected-clockwise-or-counter-clockwise 
             // Second answer, check if counterclockwise
-            if (b.X * a.Y + c.X * b.Y + a.X * c.Y < a.X * b.Y + b.X * c.Y + c.X * a.Y)
-                lUpper = lUpper.Except(new List<Point> { b });
+            if (b.X * a.Y + c.X * b.Y + a.X * c.Y <= a.X * b.Y + b.X * c.Y + c.X * a.Y)
+                upper = upper.Except(new List<Point> { b }).ToList();
             else done = true;
         }
     }
+    
+    return lower.Skip(1).Concat(upper.Skip(1));
+}
 
-    // TODO: lower half
+void DrawHull(IEnumerable<Point> points, IEnumerable<Point> convexHullPoints)
+{
+    convexHullPoints = convexHullPoints.ToList();
 
-    return lUpper;
+    List<IGrouping<int, Point>> sortedPoints = points.OrderBy(x => x.Y).Reverse().GroupBy(x => x.Y).ToList();
+    int lastLine = topBound;
+
+    foreach (IGrouping<int, Point> grouping in sortedPoints)
+    {
+        while(grouping.Key < lastLine)
+        {
+            Console.WriteLine(" ");
+            lastLine--;
+        }
+
+        IEnumerable<Point> sortedPointsOnLine = grouping.OrderBy(x => x.X).ToList();
+
+        int lastChar = leftBound - 1;
+
+        foreach (Point point in sortedPointsOnLine)
+        {
+            while (lastChar < point.X)
+            {
+                Console.Write(' ');
+                lastChar++;
+            }
+            Console.Write(convexHullPoints.Any(x => x.X == point.X && x.Y == point.Y) ? 'X' : 'O');
+            lastChar++;
+        }
+
+        Console.WriteLine();
+        lastLine--;
+    }
+
+
 }
 
 public record Point(int X, int Y);
